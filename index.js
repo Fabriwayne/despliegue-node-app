4190da696dca919dcda063b2dcc2d95049207ab7
Fabriwayne <fabriwayne11@gmail.com>
2026-09-14 15:58:03 -0400

    Subiendo cambios

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req,res)=>{
     res.json({
-        mensaje: "Hola desde Node.js desplegado en Render. Soy Fabricio Luna Rosario",
+        mensaje: "Hola desde Node.js desplegado en Render. Mi nombre es Fabricio Luna Rosario",
         ambiente: process.env.NODE_ENV || "development"
     });
 });
